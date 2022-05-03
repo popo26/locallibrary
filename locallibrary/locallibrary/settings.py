38467304cd40320a49f9140a4ee495a25423232b
popo26 <mynameispopo26@gmail.com>
2022-05-04 07:56:44 +1200

    practice deployment1

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import django_heroku
 from dotenv import load_dotenv
 from pathlib import Path
 
@@ -146,5 +147,7 @@ STATIC_URL = '/static/'
 # Simplified static file serving.
 # https://pypi.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
 
 
